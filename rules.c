0b7908f879c42619f1d652090fd8b6d5684bb43c
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-06-03 19:10:36 +0100

    Add files via upload

--- rules.c
+++ rules.c
@@ -6,7 +6,7 @@
 /*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 17:20:29 by zbabahmi          #+#    #+#             */
-/*   Updated: 2023/06/03 18:29:58 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2023/06/03 19:08:06 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	sa(t_savage *s)
 	i = s->stack_a[s->top_a];
 	s->stack_a[s->top_a] = s->stack_a[s->top_a - 1];
 	s->stack_a[s->top_a - 1] = i;
+	write(1, "sa\n", 3);
 }
 
 void	sb(t_savage *s)
@@ -28,6 +29,7 @@ void	sb(t_savage *s)
 	i = s->stack_b[s->top_b];
 	s->stack_b[s->top_b] = s->stack_b[s->top_b - 1];
 	s->stack_b[s->top_b - 1] = i;
+	write(1, "sb\n", 3);
 }
 
 void	ra(t_savage *s)
@@ -43,4 +45,21 @@ void	ra(t_savage *s)
 		i--;
 	}
 	s->stack_a[0] = prime;
+	write(1, "ra\n", 3);
+}
+
+void	rb(t_savage *s)
+{
+	int	i;
+	int	prime;
+
+	i = s->top_b;
+	prime = s->stack_b[s->top_b];
+	while (i > 0)
+	{
+		s->stack_b[i] = s->stack_b[i - 1];
+		i--;
+	}
+	s->stack_b[0] = prime;
+	write(1, "rb\n", 3);
 }
