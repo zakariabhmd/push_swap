349d342eec32c36acf03f06d1a66bceec83ac0e0
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-06-03 15:40:17 +0100

    Add files via upload

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 17:15:44 by zbabahmi          #+#    #+#             */
-/*   Updated: 2023/06/03 15:38:30 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2023/06/03 15:39:23 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ char	**fill_args(int ac, char **av);
 void	fill_stack(char **av, int ac, t_savage *s);
 char	*ft_strdup( char *s1);
 void	sa(t_savage *s);
+void	sb(t_savage *s);
 
 #endif
