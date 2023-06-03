b87407b136bf1bf7cf3193abf28cc49afc76e5ec
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-06-03 15:31:06 +0100

    Add files via upload

--- rules.c
+++ rules.c
@@ -6,12 +6,26 @@
 /*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 17:20:29 by zbabahmi          #+#    #+#             */
-/*   Updated: 2023/06/02 20:15:59 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2023/06/03 15:09:38 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	sa(t_savage *a)
+void	sa(t_savage *s)
 {
+	int	i;
+
+	i = s->stack_a[s->top_a];
+	s->stack_a[s->top_a] = s->stack_a[s->top_a - 1];
+	s->stack_a[s->top_a - 1] = i;
+}
+
+void	sb(t_savage *s)
+{
+	int	i;
+
+	i = s->stack_b[s->top_b];
+	s->stack_b[s->top_b] = s->stack_b[s->top_b - 1];
+	s->stack_b[s->top_b - 1] = i;
 }
