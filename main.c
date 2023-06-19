8d6d2908c5cb438969767fce5f08f06840c6e239
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2023-06-20 00:49:09 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 20:48:08 by zbabahmi          #+#    #+#             */
-/*   Updated: 2023/06/16 21:41:23 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2023/06/20 00:42:18 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,7 @@ char	**fill_args(int ac, char **av)
 	str = ft_strdup("");
 	while (i < ac)
 	{
+		counter(av[i], ' ');
 		str = ft_strjoin(str, " ");
 		str = ft_strjoin(str, av[i]);
 		i++;
@@ -85,6 +86,25 @@ void	fill_stack(char **av, int ac, t_savage *s)
 	freestr(str);
 	duplicate(s-> stack_a, s->size);
 }
+
+// void	check_vide(char *str)
+// {
+// 	int i;
+// 	int dex = 0;
+// 	i = 0;
+// 	while(str[i])
+// 	{
+// 		if (str[0] == '\0')
+// 			err();
+// 		if(str[i] != ' ')
+// 			dex++;
+// 		// else if(str[i] == ' ' && str[i + 1] == '\0')
+// 			// err();
+// 		i++;
+// 	}
+// 	if(dex == 0)
+// 		err();
+// }
 
 int	main(int ac, char **av)
 {
